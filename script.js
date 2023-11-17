ce2521466d812973cd87c905ba6bacc8083280d2
Igwe0001 <nwobodoemmanuel479@gmail.com>
2023-11-17 01:59:06 +0100

    change-6

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Navigation button
+const navButton = document.querySelector(".navigation-button");
+const navButtonSpan = document.querySelector(".navigation-button > span");
+const navUl = document.querySelector(".nav-ul");
+const nav = document.querySelector("nav");
 
+navButton.addEventListener("click", () => {
+  if (navButtonSpan.innerText === "menu") {
+    navButtonSpan.innerText = "close";
+  } else if (navButtonSpan.innerText === "close") {
+    navButtonSpan.innerText = "menu";
+  }
+  nav.classList.toggle("nav-change");
+  navUl.classList.toggle("ul-transform");
+});
